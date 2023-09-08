a77a90f635c942ad7063b82f069df45fc58d68c4
codedevloper13 <codedevloper13@gmail.com>
2023-09-08 23:29:22 +0530

    added the prisma build command in package .json file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,17 +16,21 @@ const Navbar = async (props: Props) => {
 			<nav className='fixed inset-x-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-350 py-2 shadow'>
 				<div className='flex items-center justify-between px-4 sm:px-8 mx-auto max-w-7xl'>
 					{/* Logo */}
-					<Link href='/gallery' className='flex items-center gap-2'>
+					<Link href='/' className='flex items-center gap-2'>
 						<Image src='/SkillWhiz.png' alt='logo' width={210} height={30} unoptimized className='pt-5 pb-5' />
 					</Link>
-					<div className='hidden sm:flex items-center space-x-4'>
-						<Link href='/create' className='text-gray-500 hover:text-gray-700'>
-							Create Course
-						</Link>
-						<Link href='/settings' className='text-gray-500 hover:text-gray-700'>
-							Settings
-						</Link>
-						<SignInButton />
+					<div className=' sm:flex items-center space-x-4'>
+						{session?.user && (
+							<>
+								<Link href='/create' className='text-gray-500 hover:text-gray-700'>
+									Create Course
+								</Link>
+								<Link href='/settings' className='text-gray-500 hover:text-gray-700'>
+									Settings
+								</Link>
+							</>
+						)}
+						<div className='flex items-center'>{session?.user ? <p>Signed In</p> : <SignInButton />}</div>
 					</div>
 				</div>
 			</nav>
